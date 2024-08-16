a9fd0d4b93aff3fd3b1450b4e5b76ba9e873284e
Mackay89 <manotomackay@gmail.com>
2024-08-16 14:20:36 -0700

    task

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-""" 9-insert_school """
+""" Script that inserts a document into a given MongoDB collection
+    and returns the ID of the inserted document """
 
 from pymongo.collection import Collection
+from typing import Any, Dict
 
-def insert_school(mongo_collection: Collection, **kwargs) -> str:
-    """Inserts a new document in the collection.
+def insert_school(mongo_collection: Collection, **kwargs: Any) -> str:
+    """ Insert a document into the MongoDB collection and return the document ID
 
     Args:
-        mongo_collection (Collection): The pymongo collection object.
-        **kwargs: Key-value pairs to be included in the new document.
-    
+        mongo_collection (Collection): The MongoDB collection object where the document will be inserted.
+        **kwargs (Any): The attributes of the document to be inserted.
+
     Returns:
-        str: The _id of the newly inserted document.
+        str: The ID of the inserted document.
     """
+    # Insert the document and retrieve the inserted ID
     result = mongo_collection.insert_one(kwargs)
     return str(result.inserted_id)
 
