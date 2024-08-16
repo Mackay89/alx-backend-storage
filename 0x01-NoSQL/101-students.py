2324d6a28879795e54aeb6a5a10074fd250cf947
Mackay89 <manotomackay@gmail.com>
2024-08-16 15:33:41 -0700

    task

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python3
-"""Module containing script that returns students sorted by average score."""
-import pymongo
-from pymongo.collection import Collection
-from typing import List, Dict, Any
+"""
+students
+"""
 
-def top_students(mongo_collection: Collection) -> List[Dict[str, Any]]:
-    """Function that returns all students sorted by average score.
 
-    Args:
-        mongo_collection (Collection): The MongoDB collection object.
-
-    Returns:
-        List[Dict[str, Any]]: A list of dictionaries with student names and their average scores.
-    """
-    cursor = mongo_collection.aggregate([
-        {"$unwind": "$topics"},  # Deconstruct the array field
-        {"$group": {
-            "_id": "$name",  # Group by student name
-            "averageScore": {"$avg": "$topics.score"}  # Calculate average score
-        }},
-        {"$sort": {"averageScore": -1}}  # Sort by average score in descending order
+def top_students(mongo_collection):
+    """ students by score """
+    return mongo_collection.aggregate([
+        {
+            "$project":
+                {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"}
+                }
+        },
+        {
+            "$sort":
+                {
+                    "averageScore": -1
+                }
+        }
     ])
-    
-    # Convert cursor to a list of dictionaries
-    return list(cursor)
 
--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,19 +1,13 @@
 #!/usr/bin/env python3
-"""Module containing function that returns the list of schools with a specific topic."""
+"""
+Where can I learn Python?
+"""
+import pymongo
 
-from pymongo.collection import Collection
-from typing import List, Dict
 
-def schools_by_topic(mongo_collection: Collection, topic: str) -> List[Dict]:
-    """Function that returns a list of schools with a specific topic.
-
-    Args:
-        mongo_collection (Collection): The MongoDB collection object.
-        topic (str): The topic to search for in the documents.
-
-    Returns:
-        List[Dict]: A list of documents (schools) that contain the specified topic.
+def schools_by_topic(mongo_collection, topic):
     """
-    cursor = mongo_collection.find({"topics": topic})
-    return list(cursor)
+    find by specific value
+    """
+    return mongo_collection.find({"topics":  {"$in": [topic]}})
 
